5bd97fb6af20520af10a7db758b8a487a757a953
Matthew Pereira <mail@matthewpereira.com>
2024-02-25 20:52:39 -0400

    Add gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,16 @@ import react             from '@vitejs/plugin-react-swc'
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
-  base: '/textsite/',
+export default defineConfig(( {command }) => {
+
+const config = {
+    plugins: [react(), viteTsconfigPaths()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/textsite/'
+  }
+
+  return config
 })
