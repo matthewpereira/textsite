e73ea38fe028840425c8e95173da9a43aadf11c6
Matthew Pereira <mail@matthewpereira.com>
2024-05-11 14:39:25 -0300

    Updates

--- helpers/textLinks.d.ts
+++ helpers/textLinks.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="react" />
 export declare const hydrateLinkElement: (linkText: string, url: string) => JSX.Element;
-export declare const parseStringForLinks: (caption: string) => string | JSX.Element;
-export default parseStringForLinks;
+export declare const formatCaption: (caption: string) => string | JSX.Element;
+export default formatCaption;
