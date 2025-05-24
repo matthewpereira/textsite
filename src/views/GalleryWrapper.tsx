d624b493901fac2397aa0f8dbc332d61e9fadda4
Matthew Pereira <mail@matthewpereira.com>
2025-05-24 13:24:40 -0300

    remove react reference

--- src/views/GalleryWrapper.tsx
+++ src/views/GalleryWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./GalleryWrapper.css";
 import { PaginationContextProvider } from '../context/PaginationContext';
 import Menu from '../components/Menu';
