e42a4a43f84d95fc1d13ad29f3689d8d05c82aa6
Matthew Pereira <mail@matthewpereira.com>
2025-06-30 21:04:18 -0300

    Correct type in decodeHtmlEntities

--- src/helpers/decodeHtmlEntities.ts
+++ src/helpers/decodeHtmlEntities.ts
@@ -1,4 +1,4 @@
-const decodeHtmlEntities = (text) => {
+const decodeHtmlEntities = (text: string) => {
     const textarea = document.createElement('textarea');
     textarea.innerHTML = text;
     return textarea.value;
