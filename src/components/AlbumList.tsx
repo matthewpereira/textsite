578ddebac8ae0c99fc95dacadf8674ca67347cf9
Matthew Pereira <mail@matthewpereira.com>
2026-01-25 10:05:57 -0400

    Protect albums route

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -1,7 +1,29 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 import Menu from './Menu.tsx';
 import AlbumSelector from "./AlbumSelector";
 
 const AlbumList = () => {
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isLoading, isAuthenticated, loginWithRedirect]);
+
+  if (isLoading) {
+    return (
+      <div style={{ fontSize: "12px", padding: "8px" }}>
+        Loading...
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
   return (
     <div>
       <Menu />
